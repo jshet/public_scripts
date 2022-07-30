4032f87dc775bfde7d3543faa8b9640f17f3e6df
jshet <59068399+jshet@users.noreply.github.com>
2022-07-30 13:25:03 -0500

    make it functional

--- roundrobin.py
+++ roundrobin.py
@@ -1,22 +1,27 @@
-people = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+people_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-if len(people) % 2 != 0:       # if there's not an even number of people, add a break time
-    people.append("-break-")
+def make_pairs(people):
+    '''
+    Create pairs by grabbing the first and last elements from the list.
+    When the round is finished, rotate all but the first player clockwise.
+    The first player never moves.
+    '''
 
-original_person_in_position_two = people[1]   # stop when this person makes it back around
+    if len(people) % 2 != 0:       # if there's not an even number of people, add a break time
+        people.append("-break-")
 
-'''
-Create pairs by grabbing the first and last elements from the list.
-When the round is finished, rotate all but the first player clockwise.
-The first player never moves.
-'''
-while True:
-    seating = people.copy()
-    pairings = []
-    while len(seating) > 0:
-        pair = (seating.pop(0), seating.pop())
-        pairings.append(pair)        
-    people.insert(1, people.pop())
-    print(pairings)
-    if people[1] == original_person_in_position_two:
-        break
+    original_person_in_position_two = people[1]   # stop when this person makes it back around
+
+    while True:
+        seating = people.copy()
+        pairings = []
+        while len(seating) > 0:
+            pair = (seating.pop(0), seating.pop())
+            pairings.append(pair)        
+        people.insert(1, people.pop())
+        print(pairings)
+        if people[1] == original_person_in_position_two:
+            break
+
+if __name__ == "__main__":
+    make_pairs(people_list)
