9b7e27f42522f566587860851da00db70fdf58e2
jshet <59068399+jshet@users.noreply.github.com>
2022-07-31 12:04:45 -0500

    test sample tests

--- roundrobin_test.py
+++ roundrobin_test.py
@@ -1,6 +1,15 @@
 import roundrobin 
 
-# tests
+# tests for roundrobin.py
 
-def test_duplicates():
-    assert roundrobin.make_pairs([1,1,2]) == 0
+def test_sample():
+    print(roundrobin.make_pairs(["Salah","TAA","Robertson","Jota"])
+
+def test_duplicate_names():
+    assert 0 == 0
+
+def test_repeated_pairings():
+    pass
+
+def test_same_person_scheduled_twice():
+    pass
