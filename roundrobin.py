4a4c330ba2442a3022f0939686345717544356c8
jshet <59068399+jshet@users.noreply.github.com>
2022-07-31 11:55:04 -0500

    replace print with return

--- roundrobin.py
+++ roundrobin.py
@@ -1,17 +1,19 @@
-people_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def make_pairs(people):
+
     '''
     Create pairs by grabbing the first and last elements from the list.
     When the round is finished, rotate all but the first player clockwise.
     The first player never moves.
+    Scheduling is completed when the original person in position two makes it back to position two.
     '''
 
     if len(people) % 2 != 0:       # if there's not an even number of people, add a break time
         people.append("-break-")
 
     original_person_in_position_two = people[1]   # stop when this person makes it back around
-
+    schedule = []
     while True:
         seating = people.copy()
         pairings = []
@@ -19,10 +21,11 @@ def make_pairs(people):
             pair = (seating.pop(0), seating.pop())
             pairings.append(pair)        
         people.insert(1, people.pop())
-        print(pairings)
+        schedule.append(pairings)
         if people[1] == original_person_in_position_two:
             break
-    return 0
+    return schedule
 
 if __name__ == "__main__":
-    make_pairs(people_list)
+    output = make_pairs(test_list)
+    print(output)
