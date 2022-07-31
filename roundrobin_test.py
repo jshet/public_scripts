b5a5e2385a1300586dc84b588d79e66838c16807
jshet <59068399+jshet@users.noreply.github.com>
2022-07-31 17:57:43 -0500

    add some tests

--- roundrobin_test.py
+++ roundrobin_test.py
@@ -3,21 +3,44 @@ import pytest
 from collections import Counter
 from itertools import combinations
 
-test_list = ["Salah","TAA","Robertson","Jota"]
+test_list = ["Salah","TAA","Robertson","Jota","VVD", "Nunez"]
 
 # tests for roundrobin.py
 
-def test_sample():
-    schedule_combinations = roundrobin.make_pairs(test_list)
-    combos = combinations(test_list)
-    assert len(combos) == len(schedule_combinations)
+def test_count_of_combinations():
+    '''Does the total number of combinations match the total number of pairings?'''
+    schedule = roundrobin.make_pairs(test_list)
+    schedule_combos = []
+    for r in schedule:
+        for p in r:
+            schedule_combos.append(p)
+    combos = list(combinations(test_list, 2))
+    assert len(combos) == len(schedule_combos)
 
 def test_duplicate_names():
+    '''Does passing a duplicate name cause an error?'''
     with pytest.raises(ValueError):
         roundrobin.make_pairs(['John', 'Jill', 'Jim', 'John'])
 
 def test_repeated_pairings():
-    pass
+    '''Are any of the pairings repeated?'''
+    schedule = roundrobin.make_pairs(test_list)
+    pairings = []
+    for round in schedule:
+        for pairing in round:
+            pairings.append(pairing)
+    c = Counter(pairings)
+    for pair in c:
+        assert c[pair] == 1
 
 def test_same_person_scheduled_twice():
-    pass
+    '''Is anyone schedule in multiple pairings for a single round?'''
+    schedule = roundrobin.make_pairs(test_list)
+    for round in schedule:
+        people_in_round = []
+        for pairing in round:
+            for person in pairing:
+                people_in_round.append(person)
+        c = Counter(people_in_round)
+        for person in c:
+            assert c[person] == 1
