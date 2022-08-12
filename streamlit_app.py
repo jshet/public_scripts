b6e47baf084742784c4115aebca333c832e469a6
Joel <jshetler@worksuites.com>
2022-08-12 18:11:20 -0500

    add text to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 def main():
-    st.write("# Hello")
+    st.markdown("# Hello")
+    st.markdwon("Please select an app from the sidebar menu.")
     
 if __name__ == "__main__":
     main()
