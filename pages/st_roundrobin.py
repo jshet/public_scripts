200535b17cb163372049a0542c966432be524deb
Joel <jshetler@worksuites.com>
2022-08-19 17:58:58 -0500

    couple of presentation edits

--- pages/st_roundrobin.py
+++ pages/st_roundrobin.py
@@ -1,29 +1,35 @@
 import streamlit as st 
 from roundrobin import make_pairs
 
-st.write("# Round Robin Scheduler")
+st.write("# Round Robin Pair Maker")
+st.write("Names go in, pairings come out, nobody gets hurt.")
 
-entry = st.text_input('Type some names here, separated by commas.', value="Bob, Sue, Jim, Kelly, Dave, Betty, Pria")
+st.markdown("## Names")
+st.markdown("Type the names of the people you would like to pair into the text input box below. Separate names with commas. Like this: 'Bob, Sally, Dave, Jill'. When all the names are added, press [Enter] or touch outside the box and the script will do things.")
+entry = st.text_input('Names:')
+st.markdown('*Example: "Pedro, Wolverine, Seinfeld, Famous Person, Third Wheel, Wingman"*')
 people = []
+
 
 for p in entry.split(','):
     people.append(p)
 
+st.markdown("## Pairings")
 if len(set(people)) == len(people) and len(people) > 1:
     schedule = make_pairs(people=people)
-    st.markdown("## Meeting schedule")
     rounds = len(schedule)
     for round in range(rounds):
-        st.markdown(f"### Round {round + 1}")
+        st.markdown(f"#### Round {round + 1}")
         output = ""
         for pair in schedule[round]:
             output+=pair[0]
             output+=" and "
             output+=pair[1]
             output+=" | "    
-        st.write(output)    
+        st.write(output[:-2])  # remove the last | character    
+    st.markdown("## Pairings by people")
     for p in people:
-        st.write(f"### Schedule for {p}")
+        st.markdown(f"#### {p}'s pairings")
         for round in range(rounds):
             for pair in schedule[round]:
                 if p in pair:
@@ -31,4 +37,6 @@ if len(set(people)) == len(people) and len(people) > 1:
                         if p == person:
                             pass
                         else:
-                            st.write(f"Round {round+1}: {person}")
+                            st.markdown(f"**Round {round+1}:** {person}")
+else:
+    st.markdown("(Pairings appear down here when names get added up there.)")
