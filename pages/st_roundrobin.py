c7f9296dd09dfe514e4b19c91bafae81e94e3b88
Joel <jshetler@worksuites.com>
2022-08-19 16:21:52 -0500

    improve output of round st_roundrobin

--- pages/st_roundrobin.py
+++ pages/st_roundrobin.py
@@ -3,7 +3,7 @@ from roundrobin import make_pairs
 
 st.write("# Round Robin Scheduler")
 
-entry = st.text_input('Type some names, separated by commas')
+entry = st.text_input('Type some names here, separated by commas.', value="Bob, Sue, Jim, Kelly, Dave, Betty, Pria")
 people = []
 
 for p in entry.split(','):
@@ -11,11 +11,24 @@ for p in entry.split(','):
 
 if len(set(people)) == len(people) and len(people) > 1:
     schedule = make_pairs(people=people)
-    st.markdown("## Here's your schedule")
+    st.markdown("## Meeting schedule")
     rounds = len(schedule)
     for round in range(rounds):
         st.markdown(f"### Round {round + 1}")
         output = ""
         for pair in schedule[round]:
-            st.markdown(pair)
-    
+            output+=pair[0]
+            output+=" and "
+            output+=pair[1]
+            output+=" | "    
+        st.write(output)    
+    for p in people:
+        st.write(f"### Schedule for {p}")
+        for round in range(rounds):
+            for pair in schedule[round]:
+                if p in pair:
+                    for person in pair:
+                        if p == person:
+                            pass
+                        else:
+                            st.write(f"Round {round+1}: {person}")
