bb6428e88868ca18d068cf02d215e5457afc59b9
joel <jshetler@worksuites.com>
2022-10-19 11:00:54 -0500

    add pandas_utils

--- pandas_utils.py
+++ pandas_utils.py
@@ -5,7 +5,6 @@ from datetime import datetime
 where_the_files_are = "C:\\data\\deskpass\\records\\"
 where_to_save_the_report = "C:\\data\\deskpass\\"
 file_list = os.listdir(where_the_files_are)
-print(file_list)
 
 def process_file(f):
     f_path = os.path.join(where_the_files_are, f)
@@ -14,10 +13,6 @@ def process_file(f):
 
 frames = [process_file(f) for f in file_list]
 combined = pd.concat(frames)
-
-# timestamp = datetime.now().timestamp()
-# date_time = datetime.fromtimestamp(timestamp)
-# timestamp_str = date_time.strftime("%Y-%m-%d_%H%M%S")
 
 timestamp_str = datetime.now().strftime("%Y-%m-%d_%H%M%S")
 save_as = os.path.join(where_to_save_the_report, f"combined_{timestamp_str}.csv")
