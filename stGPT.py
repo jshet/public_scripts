d39c2569c5db66944f4963179450acd35ac9d0f8
jshet <59068399+jshet@users.noreply.github.com>
2023-07-11 10:51:21 -0500

    Update stGPT.py

--- stGPT.py
+++ stGPT.py
@@ -2,6 +2,8 @@ import os
 import streamlit as st 
 import openai
 import apikey
+from prompts import prompts
+from datetime import datetime
 
 key = apikey.OPENAI_API_KEY
 os.environ["OPENAI_API_KEY"] = key
@@ -14,10 +16,10 @@ if 'conversation' not in st.session_state:
     st.session_state['conversation'] = []
 
 @st.cache_data
-def ask(question, temp=0.5):
+def ask(question, temp=0.5, model="gpt-3.5-turbo"):
     conversation = st.session_state['conversation']
     conversation.append(question)
-    answer = openai.ChatCompletion.create(model="gpt-3.5-turbo",temperature=temp,messages=conversation)
+    answer = openai.ChatCompletion.create(model=model,temperature=temp,messages=conversation)
     conversation.append(answer["choices"][0]["message"])
     st.session_state['conversation'] = conversation
     return answer
@@ -28,10 +30,17 @@ with col1:
     """
     ### System prompt
     """
-    system_prompt = st.text_input("System prompt:", value="You are a smart and creative writing assistant.")
-    reset = st.button("Reset")
+    selected_prompt = st.selectbox("Choose a system prompt", options=prompts.keys())
+    system_prompt = st.text_area("System prompt:", value=prompts[selected_prompt])
+    response_temp = st.slider("Response temperature:", min_value=0.0, max_value=2.0, value=0.5)
+    reset = st.button("(Re)start conversation")
     if reset:
         st.session_state['conversation'] = [{"role": "system", "content": system_prompt}]
+
+    model = "gpt-3.5-turbo"
+    model_16k = st.checkbox("Use 16k token gpt model?")
+    if model_16k:
+        model = "gpt-3.5-turbo-16k"
 
     chat_prompt = st.text_area(">>>")
     send = st.button("Send")
@@ -42,7 +51,7 @@ with col1:
 
     if send:
         question = {"role": "user", "content": chat_prompt}
-        response = ask(question)
+        response = ask(question, temp=response_temp, model=model)
         with st.expander("Response details"):
             response
 
@@ -64,3 +73,12 @@ with col2:
     if show_raw:
         msg = conversation[-1]['content']
         st.code(msg)
+    
+    save_to_file = st.button("Save conversation to file")
+    if save_to_file:
+        now = os.path.join("history", str(datetime.now()).replace(" ","-") + ".txt")
+        with open(now, 'x') as f:
+            for m in conversation:
+                f.write(f"{m['role']}: {m['content']}\n")
+                f.write("-"*80)
+                f.write("\n")
