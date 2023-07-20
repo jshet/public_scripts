470b513c044bfa43deeb07640ba4a7d83705315b
jshet <59068399+jshet@users.noreply.github.com>
2023-07-20 17:00:21 -0500

    Update gpd.py

--- gpd.py
+++ gpd.py
@@ -1,23 +1,23 @@
 import geopandas 
 import matplotlib.pyplot as plt
 
+# read a shapefile as a geopandas dataframe using the unprojected geo coordinate system (crs)
 shape_file = "shape/Elev_Contour.shp"
+gdf = geopandas.read_file(shape_file, crs="epsg:4326") 
 
-gdf = geopandas.read_file(shape_file, crs="epsg:4326")
+print(gdf.head()) # have a look at the data
+print(gdf.bounds) # print the lat/lon bounds of the geodataframe
 
-print(gdf.head())
+fig = gdf.plot(linewidth=0.5, color='k', figsize=(11,8.5)) # make the lines thin and black, set the fig w=11in and h=8.5in
 
-fig = gdf.plot(linewidth=0.5, color='k', figsize=(11,8.5))
+# set the bounds for what data to show
+minx, miny = -123.0147, 48.515
+maxx, maxy = minx+0.05, miny+0.02
+fig.set_xlim(minx, maxx)
+fig.set_ylim(miny, maxy)
 
-# minx, miny = -122.99, 48.54
-# maxx, maxy = minx+0.1, miny+0.05
-# fig.set_xlim(minx, maxx)
-# fig.set_ylim(miny, maxy)
-
-plt.tick_params(axis='both', labelsize=6)
-
-fig.ticklabel_format(useOffset=False)
-# fig.set_axis_off()
+plt.tick_params(axis='both', labelsize=6) # make the tick label font smaller
+fig.ticklabel_format(useOffset=False) # remove the scientific notation fromt he tick labels
 
 # plt.title("Title")
 # plt.grid(True)
@@ -25,3 +25,6 @@ fig.ticklabel_format(useOffset=False)
 # plt.show()
 
 plt.savefig("plot.svg", format="svg")
+
+fig.set_axis_off()
+plt.savefig("plot_no_axes.svg", format="svg")
